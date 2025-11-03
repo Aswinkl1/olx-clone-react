7af2ce783ca26b983e06839e0a6d6aeab75af07c
aswin <nuzhumaki9@gmail.com>
2025-11-03 11:18:41 +0530

    added tailwind and done some things

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="flex bg-[rgba(0,47,52,0.03)] p-3.5 justify-between">
+        <div className="flex">
+          <img
+            className="w-12"
+            src="https://statics.olx.in/external/base/img/olxLogo/olx_logo_2025.svg"
+            alt=""
+          />
+          <input type="text" name="" id="" />
+        </div>
+        <div className="flex">
+          <input className="border-2" type="text" name="" id="" />
+        </div>
+        <div className="flex">
+          <select name="" id="">
+            <option value="" selected>
+              fdfjdkkjfd
+            </option>
+            <option value="">fdfjdkkjfd</option>
+            <option value="">fdfjdkkjfd</option>
+            <option value="">fdfjdkkjfd</option>
+            <option value="">fdfjdkkjfd</option>
+            <option value="">fdfjdkkjfd</option>
+          </select>
+
+          <img src="" alt="" />
+          <a href="">Login</a>
+
+          <button>sell</button>
+        </div>
+      </nav>
+    </>
+  );
+};
